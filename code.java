985c4e05b0635965ce8fb769626e05bcffd0c3cf
Aneshsheik <sheikanesh2000@gmail.com>
2025-11-18 16:18:56 +0530

    Update code.java

--- code.java
+++ code.java
@@ -1,6 +1,18 @@
 I am adding Java code to commit the changes in my repo
 Let's paste a sample java code
-  Editing thye filr for testing webhook 
-  So, lets see how its working 
-  
-Again practicing the Jenkins 
+class Geeks
+{
+    static boolean isPrime(int n)
+    {
+        // Corner case
+        if (n <= 1)
+            return false;
+
+        // Check from 2 to n-1
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+ 
